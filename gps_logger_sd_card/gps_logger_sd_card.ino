6224fe3e9a034bd5ac6cba62ed3862cc60060ee1
Erdum <erdumadnan@gmail.com>
2022-11-10 00:20:05 +0500

    Update first test

--- gps_logger_sd_card/gps_logger_sd_card.ino
+++ gps_logger_sd_card/gps_logger_sd_card.ino
@@ -4,20 +4,82 @@
 #include <SD.h>
 #include <TinyGPSPlus.h>
 
-Adafruit_SSD1306 setupDisplay() {
-  Adafruit_SSD1306 display(128, 32, &Wire, -1);
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3c);
-  display.setTextColor(SSD1306_WHITE);
-  display.setTextSize(1);
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  return display;
-}
+Adafruit_SSD1306 oled(128, 16, &Wire, -1);
+TinyGPSPlus gps;
+
+byte hour = 0;
+byte minute = 0;
+byte second = 0;
+byte speed = 0;
+float lat = 0.0;
+float lng = 0.0;
+unsigned long prevTime = 0;
+String fileName;
+File disk;
 
 void setup() {
-  Adafruit_SSD1306 oled = setupDisplay();
+	SD.begin(10);
+	Serial.begin(9600);
+	oled.begin(SSD1306_SWITCHCAPVCC, 0x3c);
+	oled.setTextColor(SSD1306_WHITE);
+	oled.setTextSize(1);
+	oled.clearDisplay();
+	oled.setCursor(0, 0);
+  oled.println("Allah u Akbar");
+  oled.display();
+  delay(2000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+	while (Serial.available()) {
+    gps.encode(Serial.read());
+
+    if (gps.time.isValid()) {
+      hour = gps.time.hour();
+      minute = gps.time.minute();
+      second = gps.time.second();
+    }
+
+    if (gps.location.isValid()) {
+	    lat = gps.location.lat();
+	    lng = gps.location.lng();
+    }
+
+    if (gps.speed.isValid()) {
+      speed = gps.speed.kmph();
+    }
+  }
+
+	if (millis() - prevTime >= 1000 && lat != 0.0 && lng != 0.0) {
+
+		disk = SD.open("gps_data.csv", FILE_WRITE);
+		disk.print(lat);
+		disk.print(", ");
+		disk.println(lng);
+		disk.close();
+
+    prevTime = millis();
+  }
+
+	oled.clearDisplay();
+	oled.setCursor(0, 0);
+
+	if (hour < 10) oled.print('0');
+  oled.print(hour);
+  oled.print(':');
+
+  if (minute < 10) oled.print('0');
+  oled.print(minute);
+  oled.print(':');
+
+  if (second < 10) oled.print('0');
+  oled.print(second);
+
+  oled.print("  ");
+  oled.println(millis() / 1000);
+
+  oled.print(speed);
+  oled.println("kmh");
+
+  oled.display();
 }
